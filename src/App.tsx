3c2e2bc6b12f61d9efe8a34a03e452da3c13cc92
git-create-devben <benlad636@gmail.com>
2023-10-02 13:40:30 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Home } from "./Pages";
+
+const App = () => {
+  return (
+    <main className=" h-[100vh] ">
+      <Home/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,16 @@
+import { Navbar } from "../Components"
+const Home = () => {
+  return (
+    <main className=" ">
+        <Navbar/>
+        <aside>
+         {/* <h1 className=" text-2xl ">Hello</h1> */}
+        </aside>
+        <aside>
+
+        </aside>
+    </main>
+  )
+}
+
+export default Home
